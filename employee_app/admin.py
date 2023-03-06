eb6d3ae405f5c444d48bc41fec5c16986e4a2dca
AnwarT <anwartamimi90@gmail.comgit config --global user.email anwartamimi90@gmail.com>
2023-03-06 11:35:32 +0200

    project created

---
+++ employee_app/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from .models import (
+    Department,
+    Job_Application,
+    Employee,
+    Employee_Applicant,
+    Attendance)
+
+
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display =['name',]
+    fields = ['name',]
+
+
+class Job_ApplicationAdmin(admin.ModelAdmin):
+    list_display =['job_title', 'job_description', 'is_published', 'department']
+    fields =['job_title', 'job_description', 'is_published', 'department']
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'department',
+                    'is_active', 'job_application',
+                    'shift_start_at', 'shift_ends_at']
+
+admin.site.register(Employee_Applicant)
+admin.site.register(Attendance)
+admin.site.register(Department, DepartmentAdmin)
+admin.site.register(Job_Application, Job_ApplicationAdmin)
+admin.site.register(Employee, EmployeeAdmin)
---
+++ employee_app/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+
+class Department(models.Model):
+    name = models.TextField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Job_Application(models.Model):
+    job_title = models.TextField()
+    job_description = models.TextField()
+    is_published = models.BooleanField()
+    department = models.ForeignKey(Department, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.job_title
+
+
+class Employee(models.Model):
+    full_name = models.TextField()
+    birth_date = models.DateField()
+    joining_date = models.DateField(auto_now_add=True)
+    department = models.ForeignKey(
+        Department, on_delete=models.PROTECT)
+    bio = models.TextField()
+    is_active = models.BooleanField()
+    job_application = models.ForeignKey(
+        Job_Application, on_delete=models.PROTECT)
+    shift_start_at = models.DateField()
+    shift_ends_at = models.DateField()
+
+
+
+
+
+class Employee_Applicant(models.Model):
+    pass
+
+
+class Attendance(models.Model):
+    pass
+
+

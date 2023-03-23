97fc3dad49cfb663c6c395b5a805dd191a896e0f
AnwarT <anwartamimi90@gmail.comgit config --global user.email anwartamimi90@gmail.com>
2023-03-23 15:02:35 +0200

    poject created

--- employee_app/forms.py
+++ employee_app/forms.py
@@ -4,17 +4,14 @@ from .models import Employee
 
 class UserForm(forms.ModelForm):
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['shift_start_at'].required = True
+        self.fields['shift_ends_at'].required = True
+
     class Meta:
 
         model = Employee
         fields = ['shift_start_at','shift_ends_at' , 'full_name','birth_date', 'department', 'bio', 'job_application', 'is_active',  ]
 
-        #widgets = {
-         #   'shift_start_at': forms.TextInput(attrs={'readonly':'readonly'}),
-          #  'shift_ends_at': forms.TextInput(attrs={'readonly':'readonly'}),
-        #}           
-
-
-        
-def clean_shift(self):
-    return self.initial['shift_start_at', 'shift_ends_at']
+               
